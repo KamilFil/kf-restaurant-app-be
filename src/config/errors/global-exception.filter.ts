ea200e101943d6f0bcb592cc602f5752b91a6a8a
KamilFil <kamilfprog@gmail.com>
2022-11-06 19:58:33 +0100

    - Added GlobalValidation - Error

---
+++ src/config/errors/global-exception.filter.ts
@@ -0,0 +1,23 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    console.error(exception);
+    response.json({ status });
+  }
+}
