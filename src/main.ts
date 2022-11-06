6dab538cc93f5c5e71ffa6ccd84b432d108062a0
KamilFil <kamilfprog@gmail.com>
2022-11-06 18:04:52 +0100

    - Added GlobalValidationPipe Option

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true, // W środowisku produkcyjnym
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.enableCors(CorsConfig);
   await app.listen(3001);
 }
